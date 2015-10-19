fe1d2d1cbca636e411c69f601e467397226cf1db
Christopher Stewart <stewpot64@gmail.com>
2015-10-19 15:37:00 +0100

    Individual page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(title = '')
+    base_title = "QLRace"
+    if title.empty?
+      base_title
+    else
+      title + " | " + base_title
+    end
+  end
 end
