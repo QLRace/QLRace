994145a504045b6c40d09f732f1257e6e61a396e
Christopher Stewart <stewpot64@gmail.com>
2015-10-21 12:41:25 +0100

    Increased min width of pull right on mode box and check for nil/empty string in new scores

--- app/controllers/api/scores_new_controller.rb
+++ app/controllers/api/scores_new_controller.rb
@@ -5,15 +5,21 @@ class Api::ScoresNewController < Api::ApiController
 
   def new
     begin
-      player_id = Integer(params[:player_id])
-      name = params[:name]
       map = params[:map].downcase
       mode = Integer(params[:mode])
+      player_id = Integer(params[:player_id])
       time = Integer(params[:time])
-    rescue TypeError
+      name = params[:name]
+    rescue NoMethodError, TypeError
       render nothing: true, status: :bad_request
       return false
     end
+
+    if map.blank? || mode.blank? || player_id.blank? || time.blank? || name.blank?
+      render nothing: true, status: :bad_request
+      return false
+    end
+
     if Score.new_score(map, mode, player_id, time, name)
       render nothing: true, status: :ok
     else
