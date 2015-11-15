3fc824f02183e7abcc2f45645df7f2ec869cf265
Christopher Stewart <stewpot64@gmail.com>
2015-11-15 18:03:41 +0000

    Fixed Total players being only 20.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,9 +3,11 @@ class PlayersController < ApplicationController
 
   def index
     if params[:search]
-      @players = Player.search(params[:search]).order(:name).page(params[:page]).per(20)
+      players = Player.search(params[:search]).order(:name)
     else
-      @players = Player.order(:name).page(params[:page]).per(20)
-  end
+      players = Player.order(:name)
+    end
+    @total_players = players.count
+    @players = players.page(params[:page]).per(20)
   end
 end
