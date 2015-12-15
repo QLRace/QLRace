7811efe4042f14b590913d2f003546645ea8a9e6
Christopher Stewart <cstewart90@users.noreply.github.com>
2015-12-15 15:01:47 +0000

    Use ternary in title helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,7 @@
 module ApplicationHelper
   def full_title(title = '')
     base_title = 'QLRace'
-    if title.empty?
-      base_title
-    else
-      title + ' | ' + base_title
-    end
+    title.empty? ? base_title : "#{title} | #{base_title}"
   end
 
   def time_string(ms)
