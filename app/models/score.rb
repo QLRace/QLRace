35ecd7c5102278cd1441c494402cb4e9f91835ea
Christopher Stewart <cstewart90@users.noreply.github.com>
2016-01-20 20:12:47 +0000

    Reverted recent changes to map_scores. It was returning time as string which was breaking many race plugin commands.

--- app/models/score.rb
+++ app/models/score.rb
@@ -21,7 +21,7 @@ class Score < ActiveRecord::Base
   validates :player_id, uniqueness: { scope: [:map, :mode],
                                       message: 'Players may only have one record per map for each mode.' }
 
-  def rank
+  def rank_
     Score.where(map: map, mode: mode).where('time < ?', time).count + 1
   end
 
@@ -51,7 +51,7 @@ class Score < ActiveRecord::Base
     end
     medals = [0, 0, 0]
     p.scores.where(mode: mode).order(:map).each do |score|
-      rank = score.rank
+      rank = score.rank_
       scores << { map: score.map, mode: mode, rank: rank, time: score.time,
                   match_guid: score.match_guid, id: score.id, date: score.updated_at }
       medals[rank - 1] += 1 if rank.between?(1, 3)
@@ -61,7 +61,7 @@ class Score < ActiveRecord::Base
   end
 
   def self.map_scores(params)
-    mode = sanitize(mode_from_params params)
+    mode = mode_from_params params
     map = sanitize params[:map]
     query = <<-SQL
     SELECT rank() OVER (ORDER BY time), scores.id, mode, player_id, name, time,
@@ -72,7 +72,7 @@ class Score < ActiveRecord::Base
     WHERE mode = #{mode} AND map = #{map}
     ORDER BY rank, date
     SQL
-    ActiveRecord::Base.connection.execute(query).as_json
+    Score.find_by_sql query
   end
 
   private
