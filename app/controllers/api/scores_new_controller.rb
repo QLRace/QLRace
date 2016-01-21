42233db6353c4e5c2f4d98f390020144c3f18b19
Christopher Stewart <cstewart90@users.noreply.github.com>
2016-01-21 15:10:44 +0000

    Some refactoring/fixing with rubocop.

--- app/controllers/api/scores_new_controller.rb
+++ app/controllers/api/scores_new_controller.rb
@@ -6,38 +6,34 @@ class Api::ScoresNewController < Api::ApiController
     return head :not_modified if map_disabled?
 
     wr_time = WorldRecord.world_record(@score[:map], @score[:mode]).time
-    if Score.new_score(@score)
-      @score['rank'] = Score.find_by(map: @score[:map], mode: @score[:mode],
-                                     time: @score[:time]).rank_
-      @score['time_diff'] = wr_time ? @score[:time] - wr_time : 0
-
-      render json: @score
-    else
-      return head :not_modified
-    end
+    return head :not_modified unless update_score? wr_time
+    render json: @score
   end
 
-private
-
-  def check_score
+  private def check_score
+    @score = {}
     begin
-      map = params[:map].downcase
-      mode = Integer(params[:mode])
-      player_id = Integer(params[:player_id])
-      time = Integer(params[:time])
-      name = params[:name]
-      match_guid = params[:match_guid]
-      date = params[:date]
+      @score[:map] = params[:map].downcase
+      @score[:mode] = Integer(params[:mode])
+      @score[:player_id] = Integer(params[:player_id])
+      @score[:time] = Integer(params[:time])
+      @score[:name] = params[:name]
+      @score[:match_guid] = params[:match_guid]
+      @score[:date] = params[:date] if params[:date].present?
     rescue NoMethodError, TypeError, ArgumentError
       return head :bad_request
     end
-
-    @score = { map: map, mode: mode, player_id: player_id, time: time,
-               name: name, match_guid: match_guid, api_id: @user.id }
-    @score[:date] = date if date.present?
   end
 
-  def map_disabled?
+  private def update_score?(wr_time)
+    if Score.new_score(@score)
+      @score[:rank] = Score.find_by(map: @score[:map], mode: @score[:mode],
+                                    time: @score[:time]).rank_
+      @score[:time_diff] = wr_time ? @score[:time] - wr_time : 0
+    end
+  end
+
+  private def map_disabled?
     disabled_maps = %w(q3w2 q3w3 q3w5 q3w7 q3wcp1 q3wcp14 q3wcp17 q3wcp18
                        q3wcp22 q3wcp23 q3wcp5 q3wcp9 q3wxs1 q3wxs2)
     disabled_maps.include? @score[:map]
