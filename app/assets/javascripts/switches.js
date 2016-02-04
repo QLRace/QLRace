607a539cb7910e69e39c420bcae98248fceafd7e
Christopher Stewart <cstewart90@users.noreply.github.com>
2016-02-04 15:02:43 +0000

    Use single quote strings.

--- app/assets/javascripts/switches.js
+++ app/assets/javascripts/switches.js
@@ -33,12 +33,12 @@ function updateUrlParameter(uri, key, value) {
     var hash = i === -1 ? '' : uri.substr(i);
     uri = i === -1 ? uri : uri.substr(0, i);
 
-    var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+    var re = new RegExp('([?&])' + key + '=.*?(&|$)', 'i');
+    var separator = uri.indexOf('?') !== -1 ? '&' : '?';
     if (uri.match(re)) {
-        uri = uri.replace(re, '$1' + key + "=" + value + '$2');
+        uri = uri.replace(re, '$1' + key + '=' + value + '$2');
     } else {
-        uri = uri + separator + key + "=" + value;
+        uri = uri + separator + key + '=' + value;
     }
     return uri + hash; // finally append the hash as well
 }
