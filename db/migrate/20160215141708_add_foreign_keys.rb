c3f5069f12193ccc5d739ed0f9310a316105e8a3
Christopher Stewart <cstewart90@users.noreply.github.com>
2016-02-15 14:30:31 +0000

    Added foreign key constraints. https://robots.thoughtbot.com/referential-integrity-with-foreign-keys

---
+++ db/migrate/20160215141708_add_foreign_keys.rb
@@ -0,0 +1,8 @@
+class AddForeignKeys < ActiveRecord::Migration
+  def change
+    add_foreign_key 'scores', 'players',
+                    name: 'scores_player_id_fk', on_delete: :cascade
+    add_foreign_key 'world_records', 'players',
+                    name: 'world_records_player_id_fk', on_delete: :cascade
+  end
+end
