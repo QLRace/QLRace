4ec5c3b12d2f1f3312f60ec3fcc0f1801301c8ea
Christopher Stewart <cstewart90@users.noreply.github.com>
2016-10-07 09:07:43 +0100

    Fixed mina deploy script for mina 1.0.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,27 @@
-require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-require 'mina/rbenv' # for rbenv support. (http://rbenv.org)
+require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
 
 # Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
+#  domain       - The hostname to SSH to.
+#  deploy_to    - Path to deploy into.
+#  repository   - Git repo to clone from. (needed by mina/git)
+#  branch       - Branch name to deploy. (needed by mina/git)
 set :domain, 'de.qlrace.com'
 set :deploy_to, '/var/www/qlrace'
-set :repository, 'https://github.com/cstewart90/QLRace.git'
+set :repository, 'https://github.com/QLRace/QLRace.git'
 set :branch, 'master'
 
-# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
-# They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log']
-
 # Optional settings:
-set :user, 'qlrace' # Username in the server to SSH to.
-# set :forward_agent, true     # SSH forward_agent.
-set :term_mode, nil
+set :user, 'qlrace'         # Username in the server to SSH to.
+# set :port, '30000'        # SSH port number.
+# set :forward_agent, true  # SSH forward_agent.
 
-# This task is the environment that is loaded for most commands, such as
+# They will be linked in the 'deploy:link_shared_paths' step.
+# set :shared_dirs, fetch(:shared_dirs, []).push('config')
+# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# This task is the environment that is loaded all remote run commands, such as
 # `mina deploy` or `mina rake`.
 task :environment do
   # If you're using rbenv, use this to load the rbenv environment.
@@ -30,37 +29,16 @@ task :environment do
   invoke :'rbenv:load'
 end
 
-# Put any custom mkdir's in here for when `mina setup` is ran.
-# For Rails apps, we'll make some of the shared paths that are shared between
-# all releases.
-task setup: :environment do
-  queue! %(mkdir -p "#{deploy_to}/#{shared_path}/log")
-  queue! %(chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log")
-
-  queue! %(mkdir -p "#{deploy_to}/#{shared_path}/config")
-  queue! %(chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config")
-
-  queue! %(touch "#{deploy_to}/#{shared_path}/config/database.yml")
-  queue! %(touch "#{deploy_to}/#{shared_path}/config/secrets.yml")
-  queue %(echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml' and 'secrets.yml'.")
-
-  if repository
-    repo_host = repository.split(%r{@|://}).last.split(%r{:|\/}).first
-    repo_port = /:([0-9]+)/.match(repository) && /:([0-9]+)/.match(repository)[1] || '22'
-
-    queue %(
-      if ! ssh-keygen -H  -F #{repo_host} &>/dev/null; then
-        ssh-keyscan -t rsa -p #{repo_port} -H #{repo_host} >> ~/.ssh/known_hosts
-      fi
-    )
-  end
+# Put any custom commands you need to run at setup
+# All paths in `shared_dirs` and `shared_paths` will be created on their own.
+task :setup do
+  # command %{rbenv install 2.3.0}
 end
 
-desc 'Deploys the current version to the server.'
-task deploy: :environment do
-  to :before_hook do
-    # Put things to run locally before ssh
-  end
+desc "Deploys the current version to the server."
+task :deploy do
+  # uncomment this line to make sure you pushed your local branch to the remote origin
+  # invoke :'git:ensure_pushed'
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
@@ -71,16 +49,18 @@ task deploy: :environment do
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
-    to :launch do
-      queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
-      queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+    on :launch do
+      in_path(fetch(:current_path)) do
+        command %{mkdir -p tmp/}
+        command %{touch tmp/restart.txt}
+      end
     end
   end
+
+  # you can use `run :local` to run tasks on local machine before of after the deploy scripts
+  # run :local { say 'done' }
 end
 
 # For help in making your deploy script, see the Mina documentation:
 #
-#  - http://nadarei.co/mina
-#  - http://nadarei.co/mina/tasks
-#  - http://nadarei.co/mina/settings
-#  - http://nadarei.co/mina/helpers
+#  - https://github.com/mina-deploy/mina/docs
