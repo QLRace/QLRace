e388b029337f6a34554c46a654996256270905ea
Chris Stewart <cstewart90@users.noreply.github.com>
2020-06-05 21:50:15 +0100

    Updated sassc to 2.4.0 and rubocop to 0.85.0

--- app/controllers/api/scores_new_controller.rb
+++ app/controllers/api/scores_new_controller.rb
@@ -32,9 +32,7 @@ class Api::ScoresNewController < Api::ApiController
     @score[:speed_start] = params[:speed_start] if params[:speed_start].present?
     @score[:speed_end] = params[:speed_end] if params[:speed_end].present?
     @score[:speed_top] = params[:speed_top] if params[:speed_top].present?
-    if params[:speed_average].present?
-      @score[:speed_average] = params[:speed_average]
-    end
+    @score[:speed_average] = params[:speed_average] if params[:speed_average].present?
   rescue NoMethodError, TypeError, ArgumentError
     head :bad_request
   end
