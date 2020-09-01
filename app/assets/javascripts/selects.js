7262d0ab9b7429bffed029a305c6281064e1dfe3
Chris Stewart <cstewart90@users.noreply.github.com>
2020-09-01 13:35:39 +0100

    Removed unused function(updateUrlParameters) and unnecessary escape char

--- app/assets/javascripts/selects.js
+++ app/assets/javascripts/selects.js
@@ -1,22 +1,6 @@
 function urlParam(name) {
-    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(location.href);
+    var results = new RegExp('[?&]' + name + '=([^&#]*)').exec(location.href);
     return results === null ? null : results[1] || 0;
-}
-
-function updateUrlParameter(uri, key, value) {
-    // remove the hash part before operating on the uri
-    var i = uri.indexOf('#');
-    var hash = i === -1 ? '' : uri.substr(i);
-    uri = i === -1 ? uri : uri.substr(0, i);
-
-    var re = new RegExp('([?&])' + key + '=.*?(&|$)', 'i');
-    var separator = uri.indexOf('?') !== -1 ? '&' : '?';
-    if (uri.match(re)) {
-        uri = uri.replace(re, '$1' + key + '=' + value + '$2');
-    } else {
-        uri = uri + separator + key + '=' + value;
-    }
-    return uri + hash; // finally append the hash as well
 }
 
 function selects() {
