eefb77b4379f38d78ca63e5827fe0707f6d39d27
Chris Stewart <cstewart90@users.noreply.github.com>
2021-04-08 15:48:56 +0100

    Updated to rails 6.1.3.1

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,17 +11,12 @@ module Qlrace
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    config.active_record.observers = :world_record_observer
-
-    # Use memcache as default cache store.
-    config.cache_store = :mem_cache_store, {
-      namespace: 'qlrace',
-      expires_in: 1.hour
-    }
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
