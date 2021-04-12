28bff425d29a2a656cb433742c19598bc7f3e5be
Chris Stewart <cstewart90@users.noreply.github.com>
2021-04-12 17:41:42 +0100

    fixed map names in qlwc

--- app/lib/qlwc.rb
+++ app/lib/qlwc.rb
@@ -1,36 +1,34 @@
 # frozen_string_literal: true
 
-'
-QLWC 2021
+# QLWC 2021
 
-Round0 by Eksha and Kool-Aid
-Available to play: April 17th at 18:00 UTC
-Start: April 17th at 19:00 UTC
-End: April 24th at 18:00 UTC
+# Round0 by Eksha and Kool-Aid
+# Available to play: April 17th at 18:00 UTC
+# Start: April 17th at 19:00 UTC
+# End: April 24th at 18:00 UTC
 
-Round1 by mui
-Available to play: April 24th at 18:00 UTC
-Start: April 24th at 19:00 UTC
-End: May 1st at 18:00 UTC
+# Round1 by mui
+# Available to play: April 24th at 18:00 UTC
+# Start: April 24th at 19:00 UTC
+# End: May 1st at 18:00 UTC
 
-Round2 by Torp
-Available to play: May 1st at 18:00 UTC
-Start: May 1st at 19:00 UTC
-End: May 8th at 18:00 UTC
+# Round2 by Torp
+# Available to play: May 1st at 18:00 UTC
+# Start: May 1st at 19:00 UTC
+# End: May 8th at 18:00 UTC
 
-Round3 by Eksha
-Available to play: May 8th at 18:00 UTC
-Start: May 8th at 19:00 UTC
-End: May 15th at 18:00 UTC
+# Round3 by Eksha
+# Available to play: May 8th at 18:00 UTC
+# Start: May 8th at 19:00 UTC
+# End: May 15th at 18:00 UTC
 
-Round4 by Kool-Aid
-Available to play: May 15th at 18:00 UTC
-Start: May 15th at 19:00 UTC
-End: May 22th at 18:00 UTC
-'
+# Round4 by Kool-Aid
+# Available to play: May 15th at 18:00 UTC
+# Start: May 15th at 19:00 UTC
+# End: May 22th at 18:00 UTC
 
 class Qlwc
-  MAPS = %w[qlwc_round0 qlwc_round1 qlwc_round2 qlwc_round3 qlwc_round4].freeze
+  MAPS = %w[qlwc21_round0 qlwc21_round1 qlwc21_round2 qlwc21_round3 qlwc21_round4].freeze
   START_DATES = [Time.utc(2021, 4, 17, 19), Time.utc(2021, 4, 24, 19),
                  Time.utc(2021, 5, 1, 19), Time.utc(2021, 5, 8, 19),
                  Time.utc(2021, 5, 15, 19)].freeze
@@ -39,7 +37,7 @@ class Qlwc
                Time.utc(2021, 5, 22, 18)].freeze
 
   def current_map(time)
-    return nil if time < START_DATES[0] || time > END_DATES[4]
+    return nil if time < START_DATES[0] || time >= END_DATES[4]
 
     MAPS.each_with_index do |map, round|
       return map if time >= START_DATES[round] && time < END_DATES[round]
