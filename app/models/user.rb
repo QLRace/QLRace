3e3259ec30a8a6061fce87d27da6fae274cf5643
Chris Stewart <cstewart90@users.noreply.github.com>
2021-04-17 18:04:02 +0100

    Added api_key index

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+# Indexes
+#
+#  index_users_on_api_key  (api_key)
+#
 
 class User < ApplicationRecord
   before_validation :set_api_key
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_130001) do
+ActiveRecord::Schema.define(version: 2021_04_17_165757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_130001) do
     t.string "api_key", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["api_key"], name: "index_users_on_api_key"
   end
 
   create_table "world_records", id: :serial, force: :cascade do |t|
