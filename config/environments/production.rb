ab2c300e935a556764165f720336d9c7aa45635a
Chris Stewart <cstewart90@users.noreply.github.com>
2021-12-07 14:16:52 +0000

    Use memcached for production cache store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,8 +57,8 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  # Use memcached store in production.
+  config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
