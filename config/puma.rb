3bda606a5f8fa0687910150247ed164e1140d588
Chris Stewart <cstewart90@users.noreply.github.com>
2022-10-22 18:16:25 +0100

    Fixed puma socket bind

--- config/puma.rb
+++ config/puma.rb
@@ -6,7 +6,7 @@ if ENV.fetch('RAILS_ENV', 'development') == 'production'
   shared_dir = '/var/www/qlrace/shared'
   pidfile "#{shared_dir}/tmp/pids/server.pid"
   stdout_redirect "#{shared_dir}/log/puma_stdout.log", "#{shared_dir}/log/puma_stderr.log", true
-  bind puma_socket ENV.fetch('PUMA_SOCKET', "unix://#{shared_dir}/tmp/sockets/puma.sock")
+  bind ENV.fetch('PUMA_SOCKET', "unix://#{shared_dir}/tmp/sockets/puma.sock")
 end
 
 # Allow puma to be restarted by `rails restart` command.
