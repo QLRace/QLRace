b1fb5f3560e9568b3084551d3a78eb93d05fabbe
Chris Stewart <cstewart90@users.noreply.github.com>
2023-09-28 13:55:52 +0100

    Removed redundant return in api_controller.rb

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -8,7 +8,7 @@ class Api::ApiController < ApplicationController
   def authenticate
     api_key = request.headers['X-Api-Key']
     @user = User.where(api_key: api_key).first if api_key
-    return head(:unauthorized) unless @user
+    head(:unauthorized) unless @user
   end
 
   def json_request?
